5942bd0bc7effddc01592573c0e85e1ed4dedbfa
רומן סוקנר - ראש צוות <sukner_r@tel-aviv.gov.il>
2018-11-25 20:03:50 +0200

    cw

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=digitelbotwebapp-Web-Deploy.pubxml -p:Password=toxxhaWn6aQ4NsAhCwRZAS71Yd9oXWAmWKnnLjtBbqnrtTdHyibyjaTBox2P
+
